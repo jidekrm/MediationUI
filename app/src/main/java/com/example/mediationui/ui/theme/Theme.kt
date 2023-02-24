c3f14ccf95368fe6f01d1cd1604b6ec2bf0ea409
Jide Kareem <jidekrm@gmail.com>
2023-02-23 22:12:57 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mediationui/ui/theme/Theme.kt
@@ -0,0 +1,17 @@
+package com.example.mediationui.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MediationUITheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable() () -> Unit
+) {
+    MaterialTheme(
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
